07bd616668bc89f7617f8fd8476fd8bb7abee55a
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-10-16 00:15:56 +0800

    luci-app-vpnbypass: sync with upstream source

--- applications/luci-app-vpnbypass/luasrc/model/cbi/vpnbypass.lua
+++ applications/luci-app-vpnbypass/luasrc/model/cbi/vpnbypass.lua
@@ -1,18 +1,58 @@
-readmeURL = "https://github.com/openwrt/packages/blob/master/net/vpnbypass/files/README.md"
+local readmeURL = "https://github.com/openwrt/packages/blob/master/net/vpnbypass/files/README.md"
+local uci = require "luci.model.uci".cursor()
+local sys = require "luci.sys"
+local util = require "luci.util"
+local packageName = "vpnbypass"
 
-m = Map("vpnbypass", translate("VPN Bypass Settings"))
-s = m:section(NamedSection, "config", "vpnbypass")
-
--- General options
-e = s:option(Flag, "enabled", translate("Start VPNBypass service"))
-e.rmempty = false
-function e.write(self, section, value)
-	if value ~= "1" then
-		luci.sys.init.stop("vpnbypass")
+function getPackageVersion()
+	local opkgFile = "/usr/lib/opkg/status"
+	local line
+	local flag = false
+	for line in io.lines(opkgFile) do
+		if flag then
+			return line:match('[%d%.$-]+') or ""
+		elseif line:find("Package: " .. packageName:gsub("%-", "%%%-")) then
+			flag = true
+		end
 	end
-	return Flag.write(self, section, value)
+	return ""
 end
 
+local packageVersion = getPackageVersion()
+local statusText = nil 
+if packageVersion == "" then
+	statusText = translatef("%s is not installed or not found", packageName)
+end
+
+local serviceRunning, serviceEnabled = false, false
+if uci:get(packageName, "config", "enabled") == "1" then
+	serviceEnabled = true
+end
+if sys.call("iptables -t mangle -L | grep -q " .. packageName:upper()) == 0 then
+	serviceRunning = true
+end
+
+if serviceRunning then
+	statusText = translate("Running")
+else
+	statusText = translate("Stopped")
+	if not serviceEnabled then
+		statusText = translatef("%s (disabled)", statusText)
+	end
+end
+
+m = Map("vpnbypass", translate("VPN Bypass Settings"))
+
+h = m:section(NamedSection, "config", packageName, translatef("Service Status [%s %s]", packageName, packageVersion))
+ss = h:option(DummyValue, "_dummy", translate("Service Status"))
+ss.template = packageName .. "/status"
+ss.value = statusText
+if packageVersion ~= "" then
+	buttons = h:option(DummyValue, "_dummy")
+	buttons.template = packageName .. "/buttons"
+end
+
+s = m:section(NamedSection, "config", "vpnbypass", translate("VPN Bypass Rules"))
 -- Local Ports
 p1 = s:option(DynamicList, "localport", translate("Local Ports to Bypass"), translate("Local ports to trigger VPN Bypass"))
 p1.datatype    = "portrange"
@@ -30,7 +70,7 @@ p2.optional = false
 -- Local Subnets
 r1 = s:option(DynamicList, "localsubnet", translate("Local IP Addresses to Bypass"), translate("Local IP addresses or subnets with direct internet access (outside of the VPN tunnel)"))
 r1.datatype    = "ip4addr"
--- r1.placeholder = luci.ip.new(m.uci:get("network", "lan", "ipaddr"), m.uci:get("network", "lan", "netmask"))
+-- r1.placeholder = ip.new(m.uci:get("network", "lan", "ipaddr"), m.uci:get("network", "lan", "netmask"))
 r1.addremove = false
 r1.optional = false
 
@@ -46,8 +86,10 @@ d = Map("dhcp")
 s4 = d:section(TypedSection, "dnsmasq")
 s4.anonymous = true
 di = s4:option(DynamicList, "ipset", translate("Domains to Bypass"),
-    translate("Domains to be accessed directly (outside of the VPN tunnel), see ")
-		.. [[<a href="]] .. readmeURL .. [[#bypass-domains-formatsyntax" target="_blank">]]
-    .. translate("README") .. [[</a> ]] .. translate("for syntax"))
+		translatef("Domains to be accessed directly (outside of the VPN tunnel), see %sREADME%s for syntax", 
+		"<a href=\"" .. readmeURL   .. "#bypass-domains-formatsyntax" .. "\" target=\"_blank\">", "</a>"))
+function d.on_after_commit(map)
+	util.exec("/etc/init.d/dnsmasq restart >/dev/null 2>&1")
+end
 
 return m, d
