636085c5e72aa91fd28a1aedc3eb822e0d8483e3
Tianling Shen <cnsztl@immortalwrt.org>
2021-06-28 17:03:06 +0800

    luci-app-turboacc: refactor codes
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- applications/luci-app-turboacc/luasrc/controller/turboacc.lua
+++ applications/luci-app-turboacc/luasrc/controller/turboacc.lua
@@ -14,27 +14,27 @@ function index()
 end
 
 local function fastpath_status()
-	return luci.sys.call("{ [ -e /sys/module/xt_FLOWOFFLOAD/refcnt ] && [ x$(cat /sys/module/xt_FLOWOFFLOAD/refcnt 2>/dev/null) != x0 ]; } || lsmod | grep -q fast_classifier") == 0
+	return luci.sys.call("/etc/init.d/turboacc check_status fastpath") == 0
 end
 
 local function bbr_status()
-	return luci.sys.call("[ x$(cat /proc/sys/net/ipv4/tcp_congestion_control 2>/dev/null) = xbbr ]") == 0
+	return luci.sys.call("/etc/init.d/turboacc check_status bbr") == 0
 end
 
-local function fullconebat_status()
-	return luci.sys.call("[ -e /sys/module/xt_FULLCONENAT/refcnt ] && [ x$(cat /sys/module/xt_FULLCONENAT/refcnt 2>/dev/null) != x0 ]") == 0
+local function fullconenat_status()
+	return luci.sys.call("/etc/init.d/turboacc check_status fullconenat") == 0
 end
 
 local function dnscaching_status()
-	return luci.sys.call("pgrep dnscache >/dev/null") == 0
+	return luci.sys.call("/etc/init.d/turboacc check_status dns") == 0
 end
 
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 		fastpath_state = fastpath_status(),
+		fullconenat_state = fullconenat_status(),
 		bbr_state = bbr_status(),
-		fullconenat_state = fullconebat_status(),
 		dnscaching_state = dnscaching_status()
 	})
 end
--- applications/luci-app-turboacc/root/usr/share/dnscache/dnscache-while.sh
+++ applications/luci-app-turboacc/root/usr/share/dnscache/dnscache-while.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
 
 sleeptime=60
-logfile="/var/log/dnscache.file"
-dns_caching="$(uci -q get turboacc.config.dns_caching)"
-dns_caching_mode="$(uci -q get turboacc.config.dns_caching_mode 2>/dev/null)"
 
-clean_log(){
-logrow="$(grep -c "" "${logfile}")"
-if [ "${logrow}" -ge "500" ];then
-    echo "${curtime} Log条数超限，清空处理！" > "${logfile}"
-fi
+dnsconf="/var/run/dnscache/dnscache.conf"
+dnsprogram="/var/sbin/dnscache"
+logfile="/var/log/dnscache.file"
+
+dns_caching="$(uci -q get turboacc.config.dns_caching)"
+dns_caching_mode="$(uci -q get turboacc.config.dns_caching_mode)"
+
+clean_log() {
+	logrow="$(grep -c "" "${logfile}")"
+	[ "${logrow}" -lt "500" ] || echo "${curtime} Log 条数超限，清空处理！" > "${logfile}"
 }
 
 while [ "${dns_caching}" -eq "1" ];
 do
 	curtime="$(date "+%H:%M:%S")"
-	echo "${curtime} online!"
-	pidof dnscache>/dev/null || {
-		if [ "${dns_caching_mode}" = "1" ]; then
-			/var/sbin/dnscache -c "/var/etc/dnscache.conf" &
-		elif [ "${dns_caching_mode}" = "2" ]; then
-			/var/sbin/dnscache -f "/var/run/dnscache/dnscache.conf" &
-		fi
-		echo "${curtime} 重启服务！" >> ${logfile}
-}
 
 	clean_log
+
+	if pidof dnscache > "/dev/null"; then
+		echo -e "${curtime} online!" >> "${logfile}"
+	else
+		if [ "${dns_caching_mode}" = "1" ]; then
+			${dnsprogram} -c "${dnsconf}" > "${logfile}" 2>&1 &
+		elif [ "${dns_caching_mode}" = "2" ]; then
+			${dnsprogram} -f "${dnsconf}" > "${logfile}" 2>&1 &
+		elif [ "${dns_caching_mode}" = "3" ]; then
+			${dnsprogram} -o "${logfile}" -l "127.0.0.1" -p "5333" -b "tls://9.9.9.9" -f "tls://8.8.8.8" -u "${dnsconf}" --all-servers --cache --cache-min-ttl=3600 > "${logfile}" 2>&1 &
+		fi
+		echo "${curtime} 重启服务！" >> ${logfile}
+	fi
+
 	sleep "${sleeptime}"
 	continue
 done
