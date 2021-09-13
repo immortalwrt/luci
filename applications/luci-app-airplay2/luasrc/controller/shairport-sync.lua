4485313bdc7e1a8455e9b1be7be587ff57336e62
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-09-13 14:13:28 +0800

    luci-app-airplay2: export page variable
    (cherry picked from commit a74a69dc952b9690408405d9bc0d09a96d5ae370)

--- applications/luci-app-airplay2/luasrc/controller/shairport-sync.lua
+++ applications/luci-app-airplay2/luasrc/controller/shairport-sync.lua
@@ -8,7 +8,7 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "shairport-sync"}, cbi("shairport-sync"), _("AirPlay 2 Receiver"))
+	local page = entry({"admin", "services", "shairport-sync"}, cbi("shairport-sync"), _("AirPlay 2 Receiver"))
 	page.dependent = true
 	page.acl_depends = { "luci-app-airplay2" }
 	entry({"admin","services","shairport-sync","run"},call("act_status")).leaf=true
