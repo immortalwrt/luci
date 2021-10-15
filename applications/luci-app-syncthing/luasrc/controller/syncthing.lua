66ca56d3fb2cacdecb19f12f4b69624c93064380
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2021-10-16 00:01:34 +0800

    Compatible with the official Syncthing package

--- applications/luci-app-syncthing/luasrc/controller/syncthing.lua
+++ applications/luci-app-syncthing/luasrc/controller/syncthing.lua
@@ -15,7 +15,7 @@ end
 
 function act_status()
   local e={}
-  e.running=luci.sys.call("pgrep syncthing >/dev/null")==0
+  e.running=luci.sys.call("pgrep -f syncthing >/dev/null")==0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
 end
