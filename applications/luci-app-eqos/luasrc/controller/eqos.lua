aa9adc195425a8586dd3bacb79a5912344636f0a
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-24 16:58:11 +0000

    luci-app-eqos: add status bar
    Signed-off-by: Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>

--- applications/luci-app-eqos/luasrc/controller/eqos.lua
+++ applications/luci-app-eqos/luasrc/controller/eqos.lua
@@ -6,8 +6,16 @@ function index()
 	end
 	
 	local page
-
 	page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS")
 	page.dependent = true
 	page.acl_depends = { "luci-app-eqos" }
+
+	entry({"admin", "network", "eqos", "status"}, call("act_status")).leaf = true
+end
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("tc qdisc | grep htb >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
--- applications/luci-app-eqos/luasrc/model/cbi/eqos.lua
+++ applications/luci-app-eqos/luasrc/model/cbi/eqos.lua
@@ -1,17 +1,20 @@
-local ipc = require "luci.ip"
+m = Map("eqos", translate("Network speed control service"))
+m.description = translate("Limit client speed")
 
-local m = Map("eqos", translate("Network speed control service"))
+m:section(SimpleSection).template = "eqos/eqos_status"
 
-local s = m:section(TypedSection, "eqos", "")
+s = m:section(TypedSection, "eqos")
 s.anonymous = true
 
-local e = s:option(Flag, "enabled", translate("Enable"))
+e = s:option(Flag, "enabled", translate("Enable"))
 e.rmempty = false
 
-local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"), translate("Total bandwidth"))
+dl = s:option(Value, "download", translate("Download speed (Mbit/s)"))
+dl.description = translate("Total bandwidth")
 dl.datatype = "and(uinteger,min(1))"
 
-local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"), translate("Total bandwidth"))
+ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"))
+ul.description = translate("Total bandwidth")
 ul.datatype = "and(uinteger,min(1))"
 
 s = m:section(TypedSection, "device", translate("Speed limit based on IP address"))
@@ -20,9 +23,9 @@ s.anonymous = true
 s.addremove = true
 s.sortable  = true
 
-local ip = s:option(Value, "ip", translate("IP address"))
+ip = s:option(Value, "ip", translate("IP address"))
 
-ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
+luci.ip.neighbors({family = 4, dev = "br-lan"}, function(n)
 	if n.mac and n.dest then
 		ip:value(n.dest:string(), "%s (%s)" %{ n.dest:string(), n.mac })
 	end
