9dd57371abf5850945bf968ee5dcdcd6b342a963
Tianling Shen <cnsztl@immortalwrt.org>
2021-12-21 22:52:04 +0800

    luci-app-passwall: bump to 4.44
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- applications/luci-app-passwall/root/usr/share/passwall/app.sh
+++ applications/luci-app-passwall/root/usr/share/passwall/app.sh
@@ -522,7 +522,7 @@ run_socks() {
 	;;
 	ssr)
 		lua $API_GEN_SS -node $node -local_addr "0.0.0.0" -local_port $socks_port -server_host $server_host -server_port $port > $config_file
-		ln_start_bin "$(first_type ssr-local)" "ssr-local" $log_file -c "$config_file" -v
+		ln_start_bin "$(first_type ssr-local)" "ssr-local" $log_file -c "$config_file" -v -u
 	;;
 	ss)
 		lua $API_GEN_SS -node $node -local_addr "0.0.0.0" -local_port $socks_port -server_host $server_host -server_port $port -mode tcp_and_udp > $config_file
@@ -1183,11 +1183,11 @@ start_dns() {
 		[ -s "${RULES_PATH}/chnlist" ] && cp -a "${RULES_PATH}/chnlist" "${chnlist_param}"
 
 		[ -s "${RULES_PATH}/proxy_host" ] && {
-			cat "${RULES_PATH}/proxy_host" >> "${gfwlist_param}"
+			cat "${RULES_PATH}/proxy_host" | tr -s '\n' | grep -v "^#" | sort -u >> "${gfwlist_param}"
 			echolog "  | - [$?](chinadns-ng) 代理域名表合并到防火墙域名表"
 		}
 		[ -s "${RULES_PATH}/direct_host" ] && {
-			cat "${RULES_PATH}/direct_host" >> "${chnlist_param}"
+			cat "${RULES_PATH}/direct_host" | tr -s '\n' | grep -v "^#" | sort -u >> "${chnlist_param}"
 			echolog "  | - [$?](chinadns-ng) 域名白名单合并到中国域名表"
 		}
 		chnlist_param=${chnlist_param:+-m "${chnlist_param}" -M}
