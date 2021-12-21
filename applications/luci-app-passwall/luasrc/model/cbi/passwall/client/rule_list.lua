cd7455dfd04325a692165dcda379b8cf10cff3ea
Tianling Shen <cnsztl@immortalwrt.org>
2021-12-21 22:52:04 +0800

    luci-app-passwall: bump to 4.44
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 9dd57371abf5850945bf968ee5dcdcd6b342a963)

--- applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule_list.lua
+++ applications/luci-app-passwall/luasrc/model/cbi/passwall/client/rule_list.lua
@@ -14,7 +14,7 @@ s.anonymous = true
 s:tab("direct_list", translate("Direct List"))
 s:tab("proxy_list", translate("Proxy List"))
 s:tab("block_list", translate("Block List"))
-s:tab("lan_ip_list", translate("Lan Ip List"))
+s:tab("lan_ip_list", translate("Lan IP List"))
 s:tab("route_hosts", translate("Route Hosts"))
 
 ---- Direct Hosts
@@ -29,6 +29,9 @@ o.validate = function(self, value)
     local hosts= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(hosts, w) end)
     for index, host in ipairs(hosts) do
+        if host:find("#") and host:find("#") == 1 then
+            return value
+        end
         if not datatypes.hostname(host) then
             return nil, host .. " " .. translate("Not valid domain name, please re-enter!")
         end
@@ -48,6 +51,9 @@ o.validate = function(self, value)
     local ipmasks= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(ipmasks, w) end)
     for index, ipmask in ipairs(ipmasks) do
+        if ipmask:find("#") and ipmask:find("#") == 1 then
+            return value
+        end
         if not ( datatypes.ipmask4(ipmask) or datatypes.ipmask6(ipmask) ) then
             return nil, ipmask .. " " .. translate("Not valid IP format, please re-enter!")
         end
@@ -67,6 +73,9 @@ o.validate = function(self, value)
     local hosts= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(hosts, w) end)
     for index, host in ipairs(hosts) do
+        if host:find("#") and host:find("#") == 1  then
+            return value
+        end
         if not datatypes.hostname(host) then
             return nil, host .. " " .. translate("Not valid domain name, please re-enter!")
         end
@@ -86,6 +95,9 @@ o.validate = function(self, value)
     local ipmasks= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(ipmasks, w) end)
     for index, ipmask in ipairs(ipmasks) do
+        if ipmask:find("#") and ipmask:find("#") == 1 then
+            return value
+        end
         if not ( datatypes.ipmask4(ipmask) or datatypes.ipmask6(ipmask) ) then
             return nil, ipmask .. " " .. translate("Not valid IP format, please re-enter!")
         end
@@ -105,6 +117,9 @@ o.validate = function(self, value)
     local hosts= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(hosts, w) end)
     for index, host in ipairs(hosts) do
+        if host:find("#") and host:find("#") == 1 then
+            return value
+        end
         if not datatypes.hostname(host) then
             return nil, host .. " " .. translate("Not valid domain name, please re-enter!")
         end
@@ -124,6 +139,9 @@ o.validate = function(self, value)
     local ipmasks= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(ipmasks, w) end)
     for index, ipmask in ipairs(ipmasks) do
+        if ipmask:find("#") and ipmask:find("#") == 1 then
+            return value
+        end
         if not ( datatypes.ipmask4(ipmask) or datatypes.ipmask6(ipmask) ) then
             return nil, ipmask .. " " .. translate("Not valid IP format, please re-enter!")
         end
@@ -143,6 +161,9 @@ o.validate = function(self, value)
     local ipmasks= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(ipmasks, w) end)
     for index, ipmask in ipairs(ipmasks) do
+        if ipmask:find("#") and ipmask:find("#") == 1 then
+            return value
+        end
         if not datatypes.ipmask4(ipmask) then
             return nil, ipmask .. " " .. translate("Not valid IPv4 format, please re-enter!")
         end
@@ -162,6 +183,9 @@ o.validate = function(self, value)
     local ipmasks= {}
     string.gsub(value, '[^' .. "\r\n" .. ']+', function(w) table.insert(ipmasks, w) end)
     for index, ipmask in ipairs(ipmasks) do
+        if ipmask:find("#") and ipmask:find("#") == 1 then
+            return value
+        end
         if not datatypes.ipmask6(ipmask) then
             return nil, ipmask .. " " .. translate("Not valid IPv6 format, please re-enter!")
         end
