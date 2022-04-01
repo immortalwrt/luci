immortalwrt/luci

commit 700edad59694639b73a9fc1bc09ab0158b3e5ae1

optimize luci speed

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -23,62 +23,6 @@ function Dec(x) {
 	return (/^-?\d+(?:\.\d+)?$/.test(x) ? +x : NaN);
 }
 
-function IPv4(x) {
-	if (!x.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/))
-		return null;
-
-	if (RegExp.$1 > 255 || RegExp.$2 > 255 || RegExp.$3 > 255 || RegExp.$4 > 255)
-		return null;
-
-	return [ +RegExp.$1, +RegExp.$2, +RegExp.$3, +RegExp.$4 ];
-}
-
-function IPv6(x) {
-	if (x.match(/^([a-fA-F0-9:]+):(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/)) {
-		var v6 = RegExp.$1, v4 = IPv4(RegExp.$2);
-
-		if (!v4)
-			return null;
-
-		x = v6 + ':' + (v4[0] * 256 + v4[1]).toString(16)
-		       + ':' + (v4[2] * 256 + v4[3]).toString(16);
-	}
-
-	if (!x.match(/^[a-fA-F0-9:]+$/))
-		return null;
-
-	var prefix_suffix = x.split(/::/);
-
-	if (prefix_suffix.length > 2)
-		return null;
-
-	var prefix = (prefix_suffix[0] || '0').split(/:/);
-	var suffix = prefix_suffix.length > 1 ? (prefix_suffix[1] || '0').split(/:/) : [];
-
-	if (suffix.length ? (prefix.length + suffix.length > 7) : (prefix.length > 8))
-		return null;
-
-	var i, word;
-	var words = [];
-
-	for (i = 0, word = parseInt(prefix[0], 16); i < prefix.length; word = parseInt(prefix[++i], 16))
-		if (prefix[i].length <= 4 && !isNaN(word) && word <= 0xFFFF)
-			words.push(word);
-		else
-			return null;
-
-	for (i = 0; i < (8 - prefix.length - suffix.length); i++)
-		words.push(0);
-
-	for (i = 0, word = parseInt(suffix[0], 16); i < suffix.length; word = parseInt(suffix[++i], 16))
-		if (suffix[i].length <= 4 && !isNaN(word) && word <= 0xFFFF)
-			words.push(word);
-		else
-			return null;
-
-	return words;
-}
-
 var cbi_validators = {
 
 	'integer': function()
@@ -109,63 +53,69 @@ var cbi_validators = {
 
 	'ip4addr': function()
 	{
-		var m = this.match(/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?:\/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|\/(\d{1,2}))?$/);
-		return !!(m && IPv4(m[1]) && (m[2] ? IPv4(m[2]) : (m[3] ? cbi_validators.ip4prefix.apply(m[3]) : true)));
+		if (this.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(\/(\S+))?$/))
+		{
+			return (RegExp.$1 >= 0) && (RegExp.$1 <= 255) &&
+			       (RegExp.$2 >= 0) && (RegExp.$2 <= 255) &&
+			       (RegExp.$3 >= 0) && (RegExp.$3 <= 255) &&
+			       (RegExp.$4 >= 0) && (RegExp.$4 <= 255) &&
+			       ((RegExp.$6.indexOf('.') < 0)
+			          ? ((RegExp.$6 >= 0) && (RegExp.$6 <= 32))
+			          : (cbi_validators.ip4addr.apply(RegExp.$6)))
+			;
+		}
+
+		return false;
 	},
 
 	'ip6addr': function()
 	{
-		var m = this.match(/^([0-9a-fA-F:.]+)(?:\/(\d{1,3}))?$/);
-		return !!(m && IPv6(m[1]) && (m[2] ? cbi_validators.ip6prefix.apply(m[2]) : true));
-	},
+		if( this.match(/^([a-fA-F0-9:.]+)(\/(\d+))?$/) )
+		{
+			if( !RegExp.$2 || ((RegExp.$3 >= 0) && (RegExp.$3 <= 128)) )
+			{
+				var addr = RegExp.$1;
 
-	'ip4prefix': function()
-	{
-		return !isNaN(this) && this >= 0 && this <= 32;
-	},
+				if( addr == '::' )
+				{
+					return true;
+				}
 
-	'ip6prefix': function()
-	{
-		return !isNaN(this) && this >= 0 && this <= 128;
-	},
+				if( addr.indexOf('.') > 0 )
+				{
+					var off = addr.lastIndexOf(':');
 
-	'cidr': function()
-	{
-		return cbi_validators.cidr4.apply(this) ||
-			cbi_validators.cidr6.apply(this);
-	},
+					if( !(off && cbi_validators.ip4addr.apply(addr.substr(off+1))) )
+						return false;
 
-	'cidr4': function()
-	{
-		var m = this.match(/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})$/);
-		return !!(m && IPv4(m[1]) && cbi_validators.ip4prefix.apply(m[2]));
-	},
+					addr = addr.substr(0, off) + ':0:0';
+				}
 
-	'cidr6': function()
-	{
-		var m = this.match(/^([0-9a-fA-F:.]+)\/(\d{1,3})$/);
-		return !!(m && IPv6(m[1]) && cbi_validators.ip6prefix.apply(m[2]));
-	},
+				if( addr.indexOf('::') >= 0 )
+				{
+					var colons = 0;
+					var fill = '0';
 
-	'ipnet4': function()
-	{
-		var m = this.match(/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/);
-		return !!(m && IPv4(m[1]) && IPv4(m[2]));
-	},
+					for( var i = 1; i < (addr.length-1); i++ )
+						if( addr.charAt(i) == ':' )
+							colons++;
 
-	'ipnet6': function()
-	{
-		var m = this.match(/^([0-9a-fA-F:.]+)\/([0-9a-fA-F:.]+)$/);
-		return !!(m && IPv6(m[1]) && IPv6(m[2]));
-	},
+					if( colons > 7 )
+						return false;
 
-	'ip6hostid': function()
-	{
-		if (this == "eui64" || this == "random")
-			return true;
+					for( var i = 0; i < (7 - colons); i++ )
+						fill += ':0';
 
-		var v6 = IPv6(this);
-		return !(!v6 || v6[0] || v6[1] || v6[2] || v6[3]);
+					if (addr.match(/^(.*?)::(.*?)$/))
+						addr = (RegExp.$1 ? RegExp.$1 + ':' : '') + fill +
+						       (RegExp.$2 ? ':' + RegExp.$2 : '');
+				}
+
+				return (addr.match(/^(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}$/) != null);
+			}
+		}
+
+		return false;
 	},
 
 	'ipmask': function()
@@ -176,16 +126,40 @@ var cbi_validators = {
 
 	'ipmask4': function()
 	{
-		return cbi_validators.cidr4.apply(this) ||
-			cbi_validators.ipnet4.apply(this) ||
-			cbi_validators.ip4addr.apply(this);
+		var ip = this, mask = 32;
+
+		if (ip.match(/^(\S+)\/(\S+)$/))
+		{
+			ip = RegExp.$1;
+			mask = RegExp.$2;
+		}
+
+		if (!isNaN(mask) && (mask < 0 || mask > 32))
+			return false;
+
+		if (isNaN(mask) && !cbi_validators.ip4addr.apply(mask))
+			return false;
+
+		return cbi_validators.ip4addr.apply(ip);
 	},
 
 	'ipmask6': function()
 	{
-		return cbi_validators.cidr6.apply(this) ||
-			cbi_validators.ipnet6.apply(this) ||
-			cbi_validators.ip6addr.apply(this);
+		var ip = this, mask = 128;
+
+		if (ip.match(/^(\S+)\/(\S+)$/))
+		{
+			ip = RegExp.$1;
+			mask = RegExp.$2;
+		}
+
+		if (!isNaN(mask) && (mask < 0 || mask > 128))
+			return false;
+
+		if (isNaN(mask) && !cbi_validators.ip6addr.apply(mask))
+			return false;
+
+		return cbi_validators.ip6addr.apply(ip);
 	},
 
 	'port': function()
@@ -215,16 +189,15 @@ var cbi_validators = {
 	{
 		return cbi_validators.hostname.apply(this) ||
 			((ipv4only != 1) && cbi_validators.ipaddr.apply(this)) ||
-			((ipv4only == 1) && cbi_validators.ip4addr.apply(this));
+			((ipv4only == 1) && cb_validators.ip4addr.apply(this));
 	},
 
-	'hostname': function(strict)
+	'hostname': function()
 	{
 		if (this.length <= 253)
-			return (this.match(/^[a-zA-Z0-9_]+$/) != null ||
+			return (this.match(/^[a-zA-Z0-9]+$/) != null ||
 			        (this.match(/^[a-zA-Z0-9_][a-zA-Z0-9_\-.]*[a-zA-Z0-9]$/) &&
-			         this.match(/[^0-9.]/))) &&
-			       (!strict || !this.match(/^_/));
+			         this.match(/[^0-9.]/)));
 
 		return false;
 	},

modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ifaces.lua
@@ -16,7 +16,6 @@ local has_firewall = fs.access("/etc/config/firewall")
 m = Map("network", translate("Interfaces") .. " - " .. arg[1]:upper(), translate("On this page you can configure the network interfaces. You can bridge several interfaces by ticking the \"bridge interfaces\" field and enter the names of several network interfaces separated by spaces. You can also use <abbr title=\"Virtual Local Area Network\">VLAN</abbr> notation <samp>INTERFACE.VLANNR</samp> (<abbr title=\"for example\">e.g.</abbr>: <samp>eth0.1</samp>)."))
 m.redirect = luci.dispatcher.build_url("admin", "network", "network")
 m:chain("wireless")
-m:chain("luci")
 
 if has_firewall then
 	m:chain("firewall")
@@ -28,52 +27,18 @@ fw.init(m.uci)
 
 local net = nw:get_network(arg[1])
 
-local function set_ifstate(name, option, value)
-	local found = false
-
-	m.uci:foreach("luci", "ifstate", function (s)
-		if s.interface == name then
-			m.uci:set("luci", s[".name"], option, value)
-			found = true
-			return false
-		end
-	end)
-
-	if not found then
-		local sid = m.uci:add("luci", "ifstate")
-		m.uci:set("luci", sid, "interface", name)
-		m.uci:set("luci", sid, option, value)
-	end
-
-	m.uci:save("luci")
-end
-
-local function get_ifstate(name, option)
-	local val
-
-	m.uci:foreach("luci", "ifstate", function (s)
-		if s.interface == name then
-			val = m.uci:get("luci", s[".name"], option)
-			return false
-		end
-	end)
-
-	return val
-end
-
 local function backup_ifnames(is_bridge)
-	if not net:is_floating() and not get_ifstate(net:name(), "ifname") then
+	if not net:is_floating() and not m:get(net:name(), "_orig_ifname") then
 		local ifcs = net:get_interfaces() or { net:get_interface() }
 		if ifcs then
 			local _, ifn
 			local ifns = { }
 			for _, ifn in ipairs(ifcs) do
-				local wif = ifn:get_wifinet()
-				ifns[#ifns+1] = wif and wif:id() or ifn:name()
+				ifns[#ifns+1] = ifn:name()
 			end
 			if #ifns > 0 then
-				set_ifstate(net:name(), "ifname", table.concat(ifns, " "))
-				set_ifstate(net:name(), "bridge", tostring(net:is_bridge()))
+				m:set(net:name(), "_orig_ifname", table.concat(ifns, " "))
+				m:set(net:name(), "_orig_bridge", tostring(net:is_bridge()))
 			end
 		end
 	end
@@ -119,10 +84,10 @@ if m:formvalue("cbid.network.%s._switch" % net:name()) then
 		elseif net:is_floating() and not proto:is_floating() then
 			-- if we have backup data, then re-add all orphaned interfaces
 			-- from it and restore the bridge choice
-			local br = (get_ifstate(net:name(), "bridge") == "true")
+			local br = (m:get(net:name(), "_orig_bridge") == "true")
 			local ifn
 			local ifns = { }
-			for ifn in ut.imatch(get_ifstate(net:name(), "ifname")) do
+			for ifn in ut.imatch(m:get(net:name(), "_orig_ifname")) do
 				ifn = nw:get_interface(ifn)
 				if ifn and not ifn:get_network() then
 					proto:add_interface(ifn)
@@ -149,7 +114,9 @@ if m:formvalue("cbid.network.%s._switch" % net:name()) then
 		for k, v in pairs(m:get(net:name())) do
 			if k:sub(1,1) ~= "." and
 			   k ~= "type" and
-			   k ~= "ifname"
+			   k ~= "ifname" and
+			   k ~= "_orig_ifname" and
+			   k ~= "_orig_bridge"
 			then
 				m:del(net:name(), k)
 			end
@@ -272,11 +239,6 @@ if not net:is_virtual() then
 		translate("Enables the Spanning Tree Protocol on this bridge"))
 	stp:depends("type", "bridge")
 	stp.rmempty = true
-
-	igmp = s:taboption("physical", Flag, "igmp_snooping", translate("Enable <abbr title=\"Internet Group Management Protocol\">IGMP</abbr> snooping"),
-		translate("Enables IGMP snooping on this bridge"))
-	igmp:depends("type", "bridge")
-	igmp.rmempty = true
 end
 
 

commit a4a1b1024724350f771772a8a567b6d44101cdd4

modules/luci-base: fixes typo
Fixes: 700edad ("optimize luci speed")

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -189,7 +189,7 @@ var cbi_validators = {
 	{
 		return cbi_validators.hostname.apply(this) ||
 			((ipv4only != 1) && cbi_validators.ipaddr.apply(this)) ||
-			((ipv4only == 1) && cb_validators.ip4addr.apply(this));
+			((ipv4only == 1) && cbi_validators.ip4addr.apply(this));
 	},
 
 	'hostname': function()

modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/ifaces.lua
@@ -241,7 +241,7 @@ if not net:is_virtual() then
 	stp.rmempty = true
 
 	igmp = s:taboption("physical", Flag, "igmp_snooping", translate("Enable <abbr title=\"Internet Group Management Protocol\">IGMP</abbr> snooping"),
-	translate("Enables IGMP snooping on this bridge"))
+		translate("Enables IGMP snooping on this bridge"))
 	igmp:depends("type", "bridge")
 	igmp.rmempty = true
 end