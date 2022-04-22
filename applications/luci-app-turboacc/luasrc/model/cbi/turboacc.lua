6e83fddc58b1442ef9b158a915e8bb3f70e99bed
ZiMing Mo <msylgj@immortalwrt.org>
2022-04-22 15:35:33 +0800

    luci-app-turboacc: adapted the new fullconenat-nft package
    Signed-off-by: ZiMing Mo <msylgj@immortalwrt.org>

--- applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+++ applications/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -49,7 +49,7 @@ bbr_cca.default = 0
 bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
 end 
 
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FULLCONENAT.ko") then
+if nixio.fs.access("/lib/modules/" .. kernel_version .. "/nft_fullcone.ko") then
 fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
 fullcone_nat.default = 0
 fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
