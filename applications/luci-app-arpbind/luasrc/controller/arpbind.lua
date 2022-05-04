d9f9feb41f08b8ff9f7dabb21281df788236fd4d
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-04 17:18:50 +0000

    luci-app-arpbind: code alignment
    (cherry picked from commit 2c5c53918a76c4c87542f42b8ee23213a11dc610)

--- applications/luci-app-arpbind/luasrc/controller/arpbind.lua
+++ applications/luci-app-arpbind/luasrc/controller/arpbind.lua
@@ -9,6 +9,7 @@ function index()
 	if not nixio.fs.access("/etc/config/arpbind") then
 		return
 	end
+
 	local page = entry({"admin", "network", "arpbind"}, cbi("arpbind"), _("IP/MAC Binding"), 45)
 	page.dependent = true
 	page.acl_depends = { "luci-app-arpbind" }
