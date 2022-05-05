cddd747f78dd801b8cdde91c4a5086112454fcc1
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 05:11:29 +0000

    luci-app-cifs: code alignment

--- applications/luci-app-cifs/luasrc/controller/cifs.lua
+++ applications/luci-app-cifs/luasrc/controller/cifs.lua
@@ -9,9 +9,16 @@ function index()
 		return
 	end
 
-	local page
-
-	page = entry({"admin", "services", "cifs"}, cbi("cifs"), _("Mounting NAT drives"))
+	local page = entry({"admin", "services", "cifs"}, cbi("cifs"), _("Mounting NAT drives"))
 	page.dependent = true
 	page.acl_depends = { "luci-app-cifs" }
+
+	entry({"admin", "services", "cifs", "status"}, call("act_status")).leaf = true
+end
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("pidof cifsd > /dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
