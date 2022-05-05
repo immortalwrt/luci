20997b50ab2c2fca77df9c03435c8536e4c2cbd0
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 17:02:46 +0000

    luci-app-ipsec-vpnd: tidy up code

--- applications/luci-app-ipsec-vpnd/luasrc/controller/ipsec-server.lua
+++ applications/luci-app-ipsec-vpnd/luasrc/controller/ipsec-server.lua
@@ -9,13 +9,13 @@ function index()
 	page.dependent = false
 	page.acl_depends = { "luci-app-ipsec-vpnd" }
 
-	entry({"admin", "vpn", "ipsec-server"}, cbi("ipsec-server/ipsec-server"), _("IPSec VPN Server"), 80)
-	entry({"admin", "vpn", "ipsec-server","status"},call("act_status")).leaf=true
+	entry({"admin", "vpn", "ipsec-server"}, cbi("ipsec-server"), _("IPSec VPN Server"), 80)
+	entry({"admin", "vpn", "ipsec-server", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-  local e={}
-  e.running=luci.sys.call("pgrep ipsec >/dev/null")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
+	local e = {}
+	e.running = luci.sys.call("pgrep ipsec >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
