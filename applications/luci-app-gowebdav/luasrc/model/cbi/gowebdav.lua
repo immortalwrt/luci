2d9ef9d5d6117d033c74d7e27bc4fbb5910f808e
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 15:53:22 +0000

    luci-app-gowebdav: code alignment

--- applications/luci-app-gowebdav/luasrc/model/cbi/gowebdav.lua
+++ applications/luci-app-gowebdav/luasrc/model/cbi/gowebdav.lua
@@ -1,7 +1,8 @@
 -- Created By ImmortalWrt
 -- https://github.com/immortalwrt
 
-m = Map("gowebdav", translate("GoWebDav"), translate("GoWebDav is a tiny, simple, fast WevDav server."))
+m = Map("gowebdav", translate("GoWebDav"))
+m.description = translate("GoWebDav is a tiny, simple, fast WevDav server.")
 
 m:section(SimpleSection).template  = "gowebdav/gowebdav_status"
 
@@ -9,51 +10,51 @@ s = m:section(TypedSection, "gowebdav")
 s.addremove = false
 s.anonymous = true
 
-enable = s:option(Flag, "enable", translate("Enable"))
-enable.rmempty = false
+o = s:option(Flag, "enable", translate("Enable"))
+o.rmempty = false
 
-listen_port = s:option(Value, "listen_port", translate("Listen Port"))
-listen_port.placeholder = 6086
-listen_port.default     = 6086
-listen_port.datatype    = "port"
-listen_port.rmempty     = false
+o = s:option(Value, "listen_port", translate("Listen Port"))
+o.placeholder = 6086
+o.default = 6086
+o.datatype = "port"
+o.rmempty = false
 
-username = s:option(Value, "username", translate("Username"))
-username.description = translate("Leave blank to disable auth.")
-username.placeholder = user
-username.default     = user
-username.datatype    = "string"
+o = s:option(Value, "username", translate("Username"))
+o.description = translate("Leave blank to disable auth.")
+o.placeholder = user
+o.default = user
+o.datatype = "string"
 
-password = s:option(Value, "password", translate("Password"))
-password.description = translate("Leave blank to disable auth.")
-password.placeholder = pass
-password.default     = pass
-password.datatype    = "string"
-password.password    = true
+o = s:option(Value, "password", translate("Password"))
+o.description = translate("Leave blank to disable auth.")
+o.placeholder = pass
+o.default = pass
+o.datatype = "string"
+o.password = true
 
-root_dir = s:option(Value, "root_dir", translate("Root Directory"))
-root_dir.placeholder = "/mnt"
-root_dir.default     = "/mnt"
-root_dir.rmempty     = false
+o = s:option(Value, "root_dir", translate("Root Directory"))
+o.placeholder = "/mnt"
+o.default = "/mnt"
+o.rmempty = false
 
-read_only = s:option(Flag, "read_only", translate("Read-Only Mode"))
-read_only.rmempty = false
+o = s:option(Flag, "read_only", translate("Read-Only Mode"))
+o.rmempty = false
 
-allow_wan = s:option(Flag, "allow_wan", translate("Allow Access From Internet"))
-allow_wan.rmempty = false
+o = s:option(Flag, "allow_wan", translate("Allow Access From Internet"))
+o.rmempty = false
 
-use_https = s:option(Flag, "use_https", translate("Use HTTPS instead of HTTP"))
-use_https.rmempty = false
+o = s:option(Flag, "use_https", translate("Use HTTPS instead of HTTP"))
+o.rmempty = false
 
-cert_cer = s:option(Value, "cert_cer", translate("Path to Certificate"))
-cert_cer.datatype = "file"
-cert_cer:depends("use_https", 1)
+o = s:option(Value, "cert_cer", translate("Path to Certificate"))
+o.datatype = "file"
+o:depends("use_https", 1)
 
-cert_key = s:option(Value, "cert_key", translate("Path to Certificate Key"))
-cert_key.datatype = "file"
-cert_key:depends("use_https", 1)
+o = s:option(Value, "cert_key", translate("Path to Certificate Key"))
+o.datatype = "file"
+o:depends("use_https", 1)
 
-download_reg = s:option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Download Reg File\" onclick=\"window.open('https://raw.githubusercontent.com/1715173329/gowebdav/master/allow_http.reg')\" />"))
-download_reg.description = translate("Windows doesn't allow HTTP auth by default, you need to import this reg key to enable it (Reboot needed).")
+o = s:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Download Reg File\" onclick=\"window.open('https://raw.githubusercontent.com/1715173329/gowebdav/master/allow_http.reg')\" />"))
+o.description = translate("Windows doesn't allow HTTP auth by default, you need to import this reg key to enable it (Reboot needed).")
 
 return m
