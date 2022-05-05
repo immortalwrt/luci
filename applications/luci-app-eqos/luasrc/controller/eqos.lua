624ddc355caec5cdf16dc4f1c9345ea3df7f8934
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 14:55:26 +0000

    luci-app-eqos: code alignment

--- applications/luci-app-eqos/luasrc/controller/eqos.lua
+++ applications/luci-app-eqos/luasrc/controller/eqos.lua
@@ -4,10 +4,8 @@ function index()
 	if not nixio.fs.access("/etc/config/eqos") then
 		return
 	end
-	
-	local page
 
-	page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS")
+	local page = entry({"admin", "network", "eqos"}, cbi("eqos"), "EQoS")
 	page.dependent = true
 	page.acl_depends = { "luci-app-eqos" }
 end
