85bccb479bde7a24e3186e20ebd5fb594593044b
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2022-05-05 10:46:16 +0800

    luci-app-syncdial: Fixed problem that the quantity is not displayed on online interface (#138)
    * luci-app-syncdial： Fixed the problem that the quantity is not displayed on the online interface
    
    * Update syncdial.lua
    
    (cherry picked from commit ccca31a9c7e426082a7c1cb2c9de0ba733a4b1c3)

--- applications/luci-app-syncdial/luasrc/model/cbi/syncdial.lua
+++ applications/luci-app-syncdial/luasrc/model/cbi/syncdial.lua
@@ -1,6 +1,7 @@
 local e = require"nixio.fs"
 require("luci.tools.webadmin")
-local e = "mwan3 status | grep -c \"is online and tracking is active\""
+-- [[ local e = "mwan3 status | grep -c \"is online and tracking is active\"" ]]--
+local e = "mwan3 status | grep -c \"tracking is active\""
 local e = io.popen(e,"r")
 local t = e:read("*a")
 e:close()
