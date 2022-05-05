6d341aa2d65629233b34051bef806d55190e374a
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 15:53:22 +0000

    luci-app-gowebdav: code alignment
    (cherry picked from commit 67019d1f6d0a2745665627e1620c3795a5486bcb)

--- applications/luci-app-gowebdav/luasrc/controller/gowebdav.lua
+++ applications/luci-app-gowebdav/luasrc/controller/gowebdav.lua
@@ -4,17 +4,19 @@ function index()
 	if not nixio.fs.access("/etc/config/gowebdav") then
 		return
 	end
-	entry({"admin", "nas"}, firstchild(), _("NAS") , 45).dependent = false
-	local page
-	page = entry({"admin", "nas", "gowebdav"}, cbi("gowebdav"), _("GoWebDav"), 100)
+
+	entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false
+
+	local page = entry({"admin", "nas", "gowebdav"}, cbi("gowebdav"), _("GoWebDav"), 100)
 	page.dependent = true
 	page.acl_depends = { "luci-app-gowebdav" }
-	entry({"admin","nas","gowebdav","status"},call("act_status")).leaf=true
+
+	entry({"admin", "nas", "gowebdav", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-	local e={}
-	e.running=luci.sys.call("pgrep gowebdav >/dev/null")==0
+	local e = {}
+	e.running = luci.sys.call("pgrep gowebdav >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
