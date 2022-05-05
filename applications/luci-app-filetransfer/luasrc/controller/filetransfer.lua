f56f6dcf2cd675c2084cf851d9fc0bc86cabef65
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 15:27:19 +0000

    luci-app-filetransfer: tidy up code

--- applications/luci-app-filetransfer/luasrc/controller/filetransfer.lua
+++ applications/luci-app-filetransfer/luasrc/controller/filetransfer.lua
@@ -8,5 +8,8 @@ Modify: ayongwifi@126.com  www.openwrtdl.com
 module("luci.controller.filetransfer", package.seeall)
 
 function index()
-	entry({"admin", "system", "filetransfer"}, form("updownload"), _("FileTransfer"),89).acl_depends = { "luci-app-filetransfer" }
+
+	local page = entry({"admin", "system", "filetransfer"}, form("filetransfer"), _("FileTransfer"), 89)
+	page.dependent = true
+	page.acl_depends = { "luci-app-filetransfer" }
 end
