7023abd2561b10bcf97045ee37e6c214bf0dbe6b
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 04:16:29 +0000

    luci-app-beardropper: code alignment

--- applications/luci-app-beardropper/luasrc/model/cbi/beardropper/log.lua
+++ applications/luci-app-beardropper/luasrc/model/cbi/beardropper/log.lua
@@ -1,9 +1,11 @@
 f = SimpleForm("logview")
 f.reset = false
 f.submit = false
+
 t = f:field(TextValue, "conf")
 t.rmempty = true
 t.rows = 20
+
 function t.cfgvalue()
 local logs = luci.util.execi("logread | grep authpriv | grep beardropper")
 local s = ""
