cd9962104a8e97ef89ea49d4262315bf1c765bbc
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 04:42:05 +0000

    luci-app-cd8021x: code alignment
    (cherry picked from commit a0bd6464c7bb754b53a86802bba3bea8a909cf33)

--- applications/luci-app-cd8021x/luasrc/controller/cd8021x.lua
+++ applications/luci-app-cd8021x/luasrc/controller/cd8021x.lua
@@ -4,8 +4,12 @@
 module("luci.controller.cd8021x", package.seeall)
 
 function index()
-        local page = entry({"admin", "network", "cd8021x"}, cbi("cd8021x"), _("802.1x Client"))
-        page.order = 100
+	if not nixio.fs.access("/etc/config/cd8021x") then
+		return
+	end
+
+	local page = entry({"admin", "network", "cd8021x"}, cbi("cd8021x"), _("802.1x Client"))
+	page.order = 100
 	page.dependent = true
 	page.acl_depends = { "luci-app-cd8021x" }
 end
