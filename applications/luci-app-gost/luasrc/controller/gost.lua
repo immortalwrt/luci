1531a13519813c440f12009cfe1e991ded2f831b
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-05 15:46:50 +0000

    luci-app-gost: code alignment

--- applications/luci-app-gost/luasrc/controller/gost.lua
+++ applications/luci-app-gost/luasrc/controller/gost.lua
@@ -9,16 +9,16 @@ function index()
 		return
 	end
 
-	local page
-	page = entry({"admin", "services", "gost"}, cbi("gost"), _("Gost"), 100)
+	local page = entry({"admin", "services", "gost"}, cbi("gost"), _("Gost"), 100)
 	page.dependent = true
 	page.acl_depends = { "luci-app-gost" }
-	entry({"admin", "services", "gost", "status"},call("act_status")).leaf=true
+
+	entry({"admin", "services", "gost", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-	local e={}
-	e.running=luci.sys.call("pgrep gost >/dev/null")==0
+	local e = {}
+	e.running=luci.sys.call("pgrep gost >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
--- applications/luci-app-gost/luasrc/model/cbi/gost.lua
+++ applications/luci-app-gost/luasrc/model/cbi/gost.lua
@@ -4,11 +4,11 @@
 mp = Map("gost", translate("Gost"))
 mp.description = translate("A simple security tunnel written in Golang.")
 
-mp:section(SimpleSection).template = "gost/gost_status"
+mp:section(SimpleSection).template  = "gost/gost_status"
 
 s = mp:section(TypedSection, "gost")
-s.anonymous=true
-s.addremove=false
+s.anonymous = true
+s.addremove = false
 
 enable = s:option(Flag, "enable", translate("Enable"))
 enable.default = 0
