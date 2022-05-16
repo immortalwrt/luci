e9d10469afef3e14c7aaefc45523cba94a60424d
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-16 16:20:10 +0000

    luci-app-k3usb: code alignment

--- applications/luci-app-k3usb/luasrc/controller/k3usb.lua
+++ applications/luci-app-k3usb/luasrc/controller/k3usb.lua
@@ -3,8 +3,11 @@
 module("luci.controller.k3usb", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/k3usb") then
-        return
-    end
-    entry({"admin","system","k3usb"}, cbi("k3usb"), _("k3usb"),60).acl_depends = { "luci-app-k3usb" }
+	if not nixio.fs.access("/etc/config/k3usb") then
+		return
+	end
+
+	local page = entry({"admin", "system", "k3usb"}, cbi("k3usb"), _("k3usb"), 60)
+	page.dependent = true
+	page.acl_depends = { "luci-app-k3usb" }
 end
