c892d25766605325439ea3392d3ad7e25771d2c6
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-18 10:24:07 +0000

    luci-app-nfs: code alignment

--- applications/luci-app-nfs/luasrc/controller/nfs.lua
+++ applications/luci-app-nfs/luasrc/controller/nfs.lua
@@ -4,6 +4,7 @@ function index()
 	if not nixio.fs.access("/etc/config/nfs") then
 		return
 	end
+
 	local page = entry({"admin", "nas", "nfs"}, cbi("nfs"), _("NFS Manage"), 5)
 	page.dependent = true
 	page.acl_depends = { "luci-app-nfs" }
