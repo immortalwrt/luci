58b55a11c25c521c6c63e7215a0e0a51ce340bbb
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-31 14:34:58 +0000

    luci-app-nps: code alignment

--- applications/luci-app-nps/luasrc/controller/nps.lua
+++ applications/luci-app-nps/luasrc/controller/nps.lua
@@ -1,18 +1,20 @@
-module("luci.controller.nps",package.seeall)
+module("luci.controller.nps", package.seeall)
+
 function index()
-if not nixio.fs.access("/etc/config/nps")then
-return
+	if not nixio.fs.access("/etc/config/nps") then
+		return
+	end
+
+	local page = entry({"admin", "services", "nps"}, cbi("nps"), _("Nps Setting"), 100)
+	page.dependent = true
+	page.acl_depends = { "luci-app-nps" }
+
+	entry({"admin", "services", "nps", "status"}, call("act_status")).leaf = true
 end
-local e
-e=entry({"admin","services","nps"},cbi("nps"),_("Nps Setting"),100)
-e.i18n="nps"
-e.dependent=true
-e.acl_depends = { "luci-app-nps" }
-entry({"admin","services","nps","status"},call("status")).leaf=true
-end
-function status()
-local e={}
-e.running=luci.sys.call("pgrep npc > /dev/null")==0
-luci.http.prepare_content("application/json")
-luci.http.write_json(e)
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("pgrep npc > /dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
