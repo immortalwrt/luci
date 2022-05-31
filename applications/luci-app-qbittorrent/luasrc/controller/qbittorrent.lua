af201bc4fb3c061e51489e00d3e519d315d1f28c
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-31 16:32:43 +0000

    luci-app-qbittorrent: code alignment

--- applications/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+++ applications/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
@@ -1,16 +1,20 @@
-module("luci.controller.qbittorrent",package.seeall)
+module("luci.controller.qbittorrent", package.seeall)
 
 function index()
-  if not nixio.fs.access("/etc/config/qbittorrent")then
-    return
-  end
-  entry({"admin","nas","qBittorrent"},cbi("qbittorrent"),_("qBittorrent")).acl_depends = { "luci-app-qbittorrent" }
-  entry({"admin","nas","qBittorrent","status"},call("act_status")).leaf=true
+	if not nixio.fs.access("/etc/config/qbittorrent") then
+		return
+	end
+
+	local page = entry({"admin", "nas", "qBittorrent"}, cbi("qbittorrent"), _("qBittorrent"), 100)
+	page.dependent = true
+	page.acl_depends = { "luci-app-qbittorrent" }
+
+	entry({"admin", "nas", "qBittorrent", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-  local e={}
-  e.running=luci.sys.call("pgrep qbittorrent-nox >/dev/null")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
+	local e = {}
+	e.running = luci.sys.call("pgrep qbittorrent-nox >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
