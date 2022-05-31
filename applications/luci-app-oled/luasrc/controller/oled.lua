346d31fb92b1b65f83696ac157e332600992ba2d
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-31 15:05:33 +0000

    luci-app-oled: code alignment

--- applications/luci-app-oled/luasrc/controller/oled.lua
+++ applications/luci-app-oled/luasrc/controller/oled.lua
@@ -4,17 +4,17 @@ function index()
 	if not nixio.fs.access("/etc/config/oled") then
 		return
 	end
-	local page = entry({"admin", "services", "oled"}, alias("admin", "services", "oled", "setting"),_("OLED"))
-	page.order = 10
+
+	local page = entry({"admin", "services", "oled"}, cbi("oled"), _("OLED"), 10)
 	page.dependent = true
 	page.acl_depends = { "luci-app-oled" }
-	entry({"admin", "services", "oled", "status"}, call("act_status"))
-	entry({"admin", "services", "oled", "setting"}, cbi("oled/setting"),_("Setting"),30).leaf = true
+
+	entry({"admin", "services", "oled", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
-	local e={}
-	e.running = luci.sys.call("pgrep -f /usr/bin/oled > /dev/null")==0
+	local e = {}
+	e.running = luci.sys.call("pgrep -f /usr/bin/oled > /dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end 
