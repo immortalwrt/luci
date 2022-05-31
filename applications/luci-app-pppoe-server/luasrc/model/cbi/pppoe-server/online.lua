bd2214b7c6c2b0b185b304875b9d841fd804518c
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2022-05-31 16:08:44 +0000

    luci-app-pppoe-server: code alignment

--- applications/luci-app-pppoe-server/luasrc/model/cbi/pppoe-server/online.lua
+++ applications/luci-app-pppoe-server/luasrc/model/cbi/pppoe-server/online.lua
@@ -1,10 +1,9 @@
 local e = {}
 local o = require "luci.dispatcher"
-local a = luci.util.execi(
-              "/bin/busybox top -bn1 | grep 'pppd plugin rp-pppoe.so' | grep -v 'grep'")
+local a = luci.util.execi("/bin/busybox top -bn1 | grep 'pppd plugin rp-pppoe.so' | grep -v 'grep'")
+
 for t in a do
-    local a, n, h, s, o = t:match(
-                              "^ *(%d+) +(%d+) +.+rp_pppoe_sess 1:+([A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+[A-Fa-f0-9]) +.+options +(%S.-%S)%:(%S.-%S) ")
+    local a, n, h, s, o = t:match("^ *(%d+) +(%d+) +.+rp_pppoe_sess 1:+([A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+:[A-Fa-f0-9]+[A-Fa-f0-9]) +.+options +(%S.-%S)%:(%S.-%S) ")
     local t = tonumber(a)
     if t then
         e["%02i.%s" % {t, "online"}] = {
@@ -17,11 +16,12 @@ for t in a do
         }
     end
 end
+
 f = SimpleForm("processes", translate("PPPoE Server"))
 f.reset = false
 f.submit = false
-f.description = translate(
-                    "The PPPoE server is a broadband access authentication server that prevents ARP spoofing.")
+f.description = translate("The PPPoE server is a broadband access authentication server that prevents ARP spoofing.")
+
 t = f:section(Table, e, translate("Online Users"))
 t:option(DummyValue, "GATEWAY", translate("Server IP"))
 t:option(DummyValue, "CIP", translate("IP address"))
@@ -29,8 +29,10 @@ t:option(DummyValue, "MAC", translate("MAC"))
 
 kill = t:option(Button, "_kill", translate("Forced offline"))
 kill.inputstyle = "reset"
+
 function kill.write(e, t)
     null, e.tag_error[t] = luci.sys.process.signal(e.map:get(t, "PID"), 9)
     luci.http.redirect(o.build_url("admin/services/pppoe-server/online"))
 end
+
 return f
--- applications/luci-app-pppoe-server/luasrc/model/cbi/pppoe-server/users.lua
+++ applications/luci-app-pppoe-server/luasrc/model/cbi/pppoe-server/users.lua
@@ -1,6 +1,6 @@
 m = Map("pppoe-server", translate("PPPoE Server"))
-m.description = translate(
-                    "The PPPoE server is a broadband access authentication server that prevents ARP spoofing.")
+m.description = translate("The PPPoE server is a broadband access authentication server that prevents ARP spoofing.")
+
 s = m:section(TypedSection, "user", translate("Users Manager"))
 s.addremove = true
 s.anonymous = true
@@ -20,9 +20,12 @@ o = s:option(Value, "ipaddress", translate("IP address"))
 o.placeholder = translate("Automatically")
 o.datatype = "ipaddr"
 o.rmempty = true
+
 function o.cfgvalue(e, t)
     value = e.map:get(t, "ipaddress")
     return value == "*" and "" or value
 end
+
 function o.remove(e, t) Value.write(e, t, "*") end
+
 return m
