immortalwrt/luci

commit e92893b68ed8dc039b00064b000f6374569c3ba5

luci-base: network.lua: fix bridge related functions
This commit fixes these bugs:

1. Functions interface.bridge_id() and interface.bridge_stp() uses self.br field
   that is not exists at all

2. Function interface.is_bridgeport() return true only for bridge interface, not for
   bridge port interfaces.

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
(cherry picked from commit 72247ee09f17fe1f7972de29ca8bc5910a956565)

modules/luci-base/luasrc/model/network.lua
@@ -268,6 +268,7 @@ function init(cursor)
 					b.ifnames[1].bridge = b
 				end
 				_bridge[r[1]] = b
+				_interfaces[r[1]].bridge = b
 			elseif b then
 				b.ifnames[#b.ifnames+1] = _interfaces[r[2]]
 				b.ifnames[#b.ifnames].bridge = b
@@ -1286,16 +1287,16 @@ function interface.ports(self)
 end
 
 function interface.bridge_id(self)
-	if self.br then
-		return self.br.id
+	if self.dev and self.dev.bridge then
+		return self.dev.bridge.id
 	else
 		return nil
 	end
 end
 
 function interface.bridge_stp(self)
-	if self.br then
-		return self.br.stp
+	if self.dev and self.dev.bridge then
+		return self.dev.bridge.stp
 	else
 		return false
 	end
@@ -1310,7 +1311,8 @@ function interface.is_bridge(self)
 end
 
 function interface.is_bridgeport(self)
-	return self.dev and self.dev.bridge and true or false
+	return self.dev and self.dev.bridge and
+	       (self.dev.bridge.name != self:name()) and true or false
 end
 
 function interface.tx_bytes(self)

commit af77cc54e3905114377bf32f857bdd58d222be52

luci-base: network.lua: fix typo
Fixes: e92893b68

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>
(cherry picked from commit 07925b253a1b6d42dd777a1212f9b34c48e0f9f3)

modules/luci-base/luasrc/model/network.lua
@@ -1312,7 +1312,7 @@ end
 
 function interface.is_bridgeport(self)
 	return self.dev and self.dev.bridge and
-	       (self.dev.bridge.name != self:name()) and true or false
+	       (self.dev.bridge.name ~= self:name()) and true or false
 end
 
 function interface.tx_bytes(self)