be6006ef7976c4d47db801bba2866e75a6ee91a0
Tianling Shen <cnsztl@immortalwrt.org>
2023-08-14 11:23:49 +0800

    luci-app-ddns: fix start/stop service
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 17027c0bd919ff2f6ce44955fa0c29ac818c28e8)

--- applications/luci-app-ddns/htdocs/luci-static/resources/view/ddns/overview.js
+++ applications/luci-app-ddns/htdocs/luci-static/resources/view/ddns/overview.js
@@ -168,8 +168,10 @@ return view.extend({
 
 	handleToggleDDns: function(m, ev) {
 		return this.callInitAction('ddns', 'enabled')
-			.then(L.bind(function (action) { return this.callInitAction('ddns', action ? 'disable' : 'enable')}, this))
-			.then(L.bind(function (action) { return this.callInitAction('ddns', action ? 'stop' : 'start')}, this))
+			.then(L.bind(function (action) {
+				this.callInitAction('ddns', action ? 'disable' : 'enable')
+				this.callInitAction('ddns', action ? 'stop' : 'start')
+			}, this))
 			.then(L.bind(m.render, m))
 			.catch(function(e) { ui.addNotification(null, E('p', e.message)) });
 	},
