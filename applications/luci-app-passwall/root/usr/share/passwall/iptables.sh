940aecd2542553f9bdd58bba2a855a25ec6e3ccb
Interesting Gang <31793080+SakuraFallingMad@users.noreply.github.com>
2024-07-14 11:24:23 +0800

    luci-app-passwall: sync upstream
    * optimize the controller of hidding menu
    * optimize ChinaDNS-NG settings
    * fix hysteria link parsing
    * user-agent optimization
    * subscribe add sing-box domain strategy options
    * modify passwall.po
    * optimize subscription user-agent
    * fix acl when using global config
    * set the default value of dns_redirect to 0 if it is not configured

--- applications/luci-app-passwall/root/usr/share/passwall/iptables.sh
+++ applications/luci-app-passwall/root/usr/share/passwall/iptables.sh
@@ -225,6 +225,8 @@ load_acl() {
 				use_block_list=${USE_BLOCK_LIST}
 				use_gfw_list=${USE_GFW_LIST}
 				chn_list=${CHN_LIST}
+				tcp_proxy_mode=${TCP_PROXY_MODE}
+				udp_proxy_mode=${UDP_PROXY_MODE}
 			}
 
 			for i in $(cat ${TMP_ACL_PATH}/${sid}/rule_list); do
@@ -1109,7 +1111,7 @@ add_firewall_rule() {
 		$ip6t_m -I OUTPUT $(comment "mangle-OUTPUT-PSW") -o lo -j RETURN
 		insert_rule_before "$ip6t_m" "OUTPUT" "mwan3" "$(comment mangle-OUTPUT-PSW) -m mark --mark 1 -j RETURN"
 
-		[ $(config_t_get global dns_redirect) == "1" ] && {
+		[ $(config_t_get global dns_redirect "0") = "1" ] && {
 			$ipt_m -A PSW -p udp --dport 53 -j RETURN
 			$ip6t_m -A PSW -p udp --dport 53 -j RETURN
 			$ipt_n -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53 -m comment --comment "PSW_DNS_Hijack" 2>/dev/null
