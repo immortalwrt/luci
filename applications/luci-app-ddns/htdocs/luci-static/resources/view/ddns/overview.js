8c677845645098de117ad5f5e8303667f16acc71
Paul Donald <newtwen+github@gmail.com>
2026-05-26 17:26:02 +0300

    luci-app-ddns: correctness fixes
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- applications/luci-app-ddns/htdocs/luci-static/resources/view/ddns/overview.js
+++ applications/luci-app-ddns/htdocs/luci-static/resources/view/ddns/overview.js
@@ -171,9 +171,9 @@ return view.extend({
 			.catch(function(e) { ui.addNotification(null, E('p', e.message)) });
 	},
 
-	HandleStopDDnsRule(m, section_id, ev) {
+	handleStopDDnsRule(m, section_id, ev) {
 		return fs.exec('/usr/lib/ddns/dynamic_dns_lucihelper.sh',
-							[ '-S', section_id, '--', 'start' ])
+							[ '-S', section_id, '--', 'stop' ])
 			.then(L.bind(m.render, m))
 			.catch(function(e) { ui.addNotification(null, E('p', e.message)) });
 	},
@@ -551,7 +551,7 @@ return view.extend({
 				},
 				stop_opt = {
 					'class': 'cbi-button cbi-button-neutral stop',
-					'click': ui.createHandlerFn(_this, 'HandleStopDDnsRule', m, section_id),
+					'click': ui.createHandlerFn(_this, 'handleStopDDnsRule', m, section_id),
 					'title': _('Stop this service'),
 				};
 
