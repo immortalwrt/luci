ad487f931f75bceedddea23128ba9879501a4b5a
Paul Donald <newtwen+github@gmail.com>
2026-05-27 16:02:53 +0300

    luci-mod-network: convert tag strings i18n
    matchtags
    
    closes #8603
    
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/dhcp.js
@@ -647,13 +647,13 @@ return view.extend({
 
 		// Match Tags
 		o = tagstab.taboption('matchtags', form.SectionValue, '__tags__', form.TableSection, 'tag', null,
-			_(`A ${tagcodestring} is an alphanumeric label.`) + ' ' + _(`They are attached to a DHCP client or transaction.`) + '<br />' +
-			_(`dnsmasq conditionally applies chosen DHCP options when a specific ${tagcodestring} is encountered.`) + '<br />' +
-			_(`In other words: "This ${tagcodestring} gets these ${tag_named_ov_string}".`) + '<br />' +
-			_(`${tagcodestring}s do not do anything by themselves. They are labels that other directives test against.`) + '<br />' +
-			_(`Note: invalid ${tag_named_ov_string} combinations may cause dnsmasq to crash silently.`) + '<br /><br />' +
-			_(`Prepend a ${tagcodestring} with ${exclamationmark_invert} to invert their domain of application, e.g. to send options to a host lacking a ${tagcodestring}.`) + '<br /><br />' +
-			_(`Use the %s button to add a new ${tagcodestring}.`).format( _(`<em>${addtag}</em>`) ) );
+			_('A %s is an alphanumeric label.', 'A tag is an alphanumeric label').format(tagcodestring) + ' ' + _('They filter which options apply to which hosts.') + '<br />' +
+			_('dnsmasq conditionally applies chosen DHCP options when a specific %s is encountered.', 'when a specific tag is encountered').format(tagcodestring) + '<br />' +
+			_('In other words: "This %s gets these %s".', 'this tag gets these options').format(tagcodestring, tag_named_ov_string) + '<br />' +
+			_('A %s does not do anything by itself. It is a label that other directives test against.', 'A tag does not do anything by itself').format(tagcodestring) + '<br />' +
+			_('Note: invalid %s combinations may cause dnsmasq to crash silently.', 'invalid tag combinations may cause crashes').format(tag_named_ov_string) + '<br /><br />' +
+			_('Prepend a %s with %s to invert their domain of application, e.g. to send options to a host lacking a %s.', 'prepend a tag with ! to invert their meaning').format(tagcodestring, exclamationmark_invert, tagcodestring) + '<br /><br />' +
+			_('Use the %s button to add a new %s.', 'use the add button to add a new tag').format( _(`<em>${addtag}</em>`), tagcodestring ) );
 		ss = o.subsection;
 		ss.placeholder = _('tag name');
 		ss.sortable = true;
