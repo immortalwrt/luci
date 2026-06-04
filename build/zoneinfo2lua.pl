2f0c57563b68c1ff81dd3ac0162f98d7fb5a91da
Hannu Nyman <hannu.nyman@iki.fi>
2026-06-04 21:42:57 +0300

    build: modify zoneinfo2lua.pl to handle luci-lua-runtime
    Modify zoneinfo2lua.pl script to handle the old lua timezone files
    that have been moved from luci-base to luci-lua-runtime.
    
    (modern LuCI uses ucode based timezone files, so this is a fallback
    for old lua based apps.)
    
    Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>
    (cherry picked from commit 95a2399f9159faa5556701aaecf787b7a8a763c2)

--- build/zoneinfo2lua.pl
+++ build/zoneinfo2lua.pl
@@ -13,7 +13,7 @@ use strict;
 my %TZ;
 
 my $tzdin  = $ARGV[0] || "/usr/share/zoneinfo";
-my $tzdout = $ARGV[1] || "./modules/luci-base/luasrc/sys/zoneinfo";
+my $tzdout = $ARGV[1] || "./modules/luci-lua-runtime/luasrc/sys/zoneinfo";
 
 local $/ = "\012";
 open( ZTAB, "< $tzdin/zone.tab" ) || die "open($tzdin/zone.tab): $!";
